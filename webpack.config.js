8227cf6633f03cb51e19a1c6ffad7468771fa6bb
chriszdeux <chriszdeux@gmail.com>
2020-10-14 22:11:38 -0700

    connecting routes :)

--- webpack.config.js
+++ webpack.config.js
@@ -25,13 +25,13 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebpackPlugin([
+    new HtmlWebpackPlugin(
       {
         inject: true,
         template: './public/index.html',
         filename: './index.html',
       }
-    ])
+    )
     
   ]
 }
