e3bcf526b0945e5caa3cb59ca33f1ab6358efddb
chriszdeux <chriszdeux@gmail.com>
2020-10-15 14:10:50 -0700

    done

---
+++ src/utils/getData.js
@@ -0,0 +1,16 @@
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const getData = async ( id ) => {
+  const urlAPI = id ? `${API}${id}` : API;
+
+  try {
+    const response = await fetch(urlAPI);
+    const data = await response.json();
+    
+    return data
+  } catch (error) {
+      console.log('fetch Error', error)
+  }
+}
+
+export default getData;
